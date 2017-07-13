932ba3c6b30e297feb3ca471c28a218f12c3e02c
rodshokrian <rodshokrian@gmail.com>
2017-07-13 10:05:19 -0700

    2nd day

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {fetchAllPokemon} from './util/api_util';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
+import configureStore from './store/store';
+import {selectAllPokemon} from './reducers/selectors';
+import Root from './components/root';
+import { HashRouter, Route } from 'react-router-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  const store = configureStore();
+  //test
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  ReactDOM.render(<Root store={store}/>, rootEl);
+});
